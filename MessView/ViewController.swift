3fd6dc23380193b697be5579ccf161420a39bf04
Vlad <tretyak.vladik@gmail.com>
2018-12-19 18:21:45 +0200

    Initial Commit

---
+++ MessView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessView
+//
+//  Created by Влад Третьяк on 12/19/18.
+//  Copyright © 2018 Влад Третьяк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
